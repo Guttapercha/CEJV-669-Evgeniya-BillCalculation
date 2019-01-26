997b4612c4763528d43f75837a7bb5979da67ebb
Guttapercha <chitosan@inbox.ru>
2019-01-26 01:53:38 -0500

    Bill template

--- app/src/main/java/com/example/evgeniya/calculator/MainActivity.java
+++ app/src/main/java/com/example/evgeniya/calculator/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.evgeniya.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btn_pressed(View v) {
-        EditText n1 = findViewById(R.id.number1);
-        EditText n2 = findViewById(R.id.number2);
+        EditText n1 = findViewById(R.id.diners);
+        EditText n2 = findViewById(R.id.price);
         EditText result = findViewById(R.id.number_result);
         int addition;
         addition = Integer.parseInt(n1.getText().toString()) + Integer.parseInt(n2.getText().toString());
