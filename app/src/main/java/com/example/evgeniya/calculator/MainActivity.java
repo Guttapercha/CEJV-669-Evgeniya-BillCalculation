40d266ba748f1cacd440ebf1d3c14b3ca8745df2
Guttapercha <chitosan@inbox.ru>
2019-01-28 01:17:04 -0500

    final app - bill calculation

--- app/src/main/java/com/example/evgeniya/calculator/MainActivity.java
+++ app/src/main/java/com/example/evgeniya/calculator/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,13 +28,47 @@ public class MainActivity extends AppCompatActivity {
         EditText QST = findViewById(R.id.lbl_QST);
         EditText tips = findViewById(R.id.lbl_tip);
         EditText result = findViewById(R.id.result);
+        TextView error = findViewById(R.id.lbl_error);
 
-        totalPerPerson.setText(String.valueOf(Double.parseDouble(price.getText().toString())*
-                (1 + 0.05 + 0.0975 + Double.parseDouble(tip.getText().toString())/100)));
-        GST.setText(String.valueOf(Double.parseDouble(price.getText().toString())*0.05));
-        QST.setText(String.valueOf(Double.parseDouble(price.getText().toString())*0.09975));
-        tips.setText(String.valueOf(Double.parseDouble(price.getText().toString())*
-                (Double.parseDouble(tip.getText().toString())/100)));
-        result.setText(String.valueOf(Double.parseDouble(totalPerPerson.getText().toString())*Integer.parseInt(diners.getText().toString())));
+         if (price.getText().toString().equals("") || diners.getText().toString().equals("") || tip.getText().toString().equals("")) {
+             error.setText("Do not leave empty fields");
+         } else {
+             error.setText("");
+             totalPerPerson.setText(String.format(Locale.getDefault(), "%.02f", Double.parseDouble(price.getText().toString())*
+                     (1 + 0.05 + 0.09975 + Double.parseDouble(tip.getText().toString())/100)));
+             GST.setText(String.format(Locale.getDefault(),"%.02f", Double.parseDouble(price.getText().toString())*0.05));
+             QST.setText(String.format(Locale.getDefault(),"%.02f", Double.parseDouble(price.getText().toString())*0.09975));
+
+             tips.setText(String.format(Locale.getDefault(),"%.02f", Double.parseDouble(price.getText().toString())*
+                     (Double.parseDouble(tip.getText().toString())/100)));
+             result.setText(String.format(Locale.getDefault(),"%.02f", Double.parseDouble(totalPerPerson.getText().toString())*Integer.parseInt(diners.getText().toString())));
+         }
+
+        }
+
+    public void btn_pressed_clear(View v) {
+        TextView error = findViewById(R.id.lbl_error);
+        error.setText("Do not leave empty fields");
+        //data input
+        EditText diners = findViewById(R.id.diners);
+        EditText price = findViewById(R.id.price);
+        EditText tip = findViewById(R.id.tip);
+
+        //data output
+        EditText totalPerPerson = findViewById(R.id.lbl_totalPerPerson);
+        EditText GST = findViewById(R.id.lbl_GST);
+        EditText QST = findViewById(R.id.lbl_QST);
+        EditText tips = findViewById(R.id.lbl_tip);
+        EditText result = findViewById(R.id.result);
+
+        totalPerPerson.setText("");
+        GST.setText("");
+        QST.setText("");
+        tips.setText("");
+        result.setText("");
+        diners.setText("");
+        price.setText("");
+        tip.setText("");
+
     }
 }
